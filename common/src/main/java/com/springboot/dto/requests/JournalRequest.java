cb6d6bd07e8a88de902924b5108141f0cca6acf9
Jayesh Shinde <jayeshshinde@Jayeshs-MacBook-Pro.local>
2025-09-07 15:54:13 +0900

    feat: added a test case for post with assertions and request header

--- common/src/main/java/com/springboot/dto/requests/JournalRequest.java
+++ common/src/main/java/com/springboot/dto/requests/JournalRequest.java
@@ -4,6 +4,7 @@ package com.springboot.dto.requests;
 import com.springboot.dto.enums.JournalStatus;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.util.List;
 
 @Data
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class JournalRequest {
     @Size(min = 5, max = 100)
     private String reference;
--- ledger-service/src/test/java/com/springboot/JournalControllerTest.java
+++ ledger-service/src/test/java/com/springboot/JournalControllerTest.java
@@ -1,7 +1,13 @@
 package com.springboot;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.dto.enums.JournalStatus;
+import com.springboot.dto.requests.JournalRequest;
 import com.springboot.dto.response.AccountBalances;
+import com.springboot.dto.response.JournalLineResponse;
+import com.springboot.dto.response.JournalResponse;
 import com.springboot.service.JournalService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,13 +15,17 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,6 +38,9 @@ public class JournalControllerTest {
     @MockitoBean
     private JournalService journalService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     public void getAccountBalance() throws Exception {
         UUID testAccountId = UUID.fromString("550e8400-e29b-41d4-a716-446655440000");
@@ -38,5 +51,60 @@ public class JournalControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.accountId", is(testAccountId.toString())))
                 .andExpect(jsonPath("$.balance", is(1000000.00)));
+    }
+
+    @Test
+    public void createJournal() throws Exception {
+
+        List<JournalLineResponse> journalLineResponses = new ArrayList<>();
+        JournalLineResponse journalLineResponse = new
+                JournalLineResponse(UUID.fromString("550e8400-e29b-41d4-a716-446655440001"),
+                UUID.fromString("550e8400-e29b-41d4-a716-446655440000"),
+                BigDecimal.valueOf(100),
+                "CREDIT");
+        JournalLineResponse journalLineResponse1 = new
+                JournalLineResponse(UUID.fromString("550e8400-e29b-41d4-a716-446655440002"),
+                UUID.fromString("550e8400-e29b-41d4-a716-446655440000"),
+                BigDecimal.valueOf(100),
+                "DEBIT");
+        journalLineResponses.add(journalLineResponse);
+        journalLineResponses.add(journalLineResponse1);
+        JournalResponse journalResponse =
+                new JournalResponse(UUID.fromString("550e8400-e29b-41d4-a716-446655440000"),
+                        "sample transfer",
+                        JournalStatus.POSTED,
+                        journalLineResponses
+                );
+        JournalRequest journalRequest =
+                new JournalRequest("sample transfer",
+                        JournalStatus.POSTED,
+                        new ArrayList<>());
+        when(journalService.createJournal(journalRequest, "8"))
+                .thenReturn(journalResponse);
+        String requestBody = objectMapper.writeValueAsString(journalRequest);
+        MvcResult mvcResult = mockMvc.perform(post("/api/journals")
+                        .header("Idempotency-Key", 8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                //.andExpect(status().isCreated())
+                .andReturn();
+        //System.out.println(mvcResult.getResponse().getContentAsString());
+        JournalResponse journalResponse1 =
+                objectMapper.readValue(mvcResult.getResponse().getContentAsString(), JournalResponse.class);
+        Assertions.assertEquals(201, mvcResult.getResponse().getStatus());
+
+        Assertions.assertEquals("POSTED", journalResponse1.getJournalStatus().toString());
+        Assertions.assertEquals(UUID.fromString("550e8400-e29b-41d4-a716-446655440000"),
+                journalResponse1.getJournalLineResponses().get(0).getAccountId());
+        Assertions.assertEquals(BigDecimal.valueOf(100),
+                journalResponse1.getJournalLineResponses().get(0).getAmount());
+        Assertions.assertEquals("CREDIT",
+                journalResponse1.getJournalLineResponses().get(0).getEntryType());
+        Assertions.assertEquals(UUID.fromString("550e8400-e29b-41d4-a716-446655440000"),
+                journalResponse1.getJournalLineResponses().get(1).getAccountId());
+        Assertions.assertEquals(BigDecimal.valueOf(100),
+                journalResponse1.getJournalLineResponses().get(1).getAmount());
+        Assertions.assertEquals("DEBIT",
+                journalResponse1.getJournalLineResponses().get(1).getEntryType());
     }
 }
